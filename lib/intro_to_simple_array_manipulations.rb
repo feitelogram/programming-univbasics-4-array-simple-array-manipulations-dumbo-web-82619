d07415d61f3488512a633b321c76810bd79f61bc
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-04 01:28:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,8 +42,8 @@ def using_delete (array, string)
   array.delete(string)
 end
 
-def using_delete_at (array, integer)
-  array.delete (integer)
+def using_delete_at (array, number)
+  array.delete (number.integer)
 end
 
   
